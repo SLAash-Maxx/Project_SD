fb6d4fc40cd93e545fef7467823c50ead8304e63
Anuhas Diwyanjana <anuhasdiwyanjanapubg@gmail.com>
2026-04-14 16:06:09 +0530

    System admin

--- MentorNode/Controllers/HomeController.cs
+++ MentorNode/Controllers/HomeController.cs
@@ -1,25 +1,32 @@
-using MentorNode.Models;
+using BlindMatchPAS.Data;
+using BlindMatchPAS.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using Microsoft.EntityFrameworkCore;
 
-namespace MentorNode.Controllers
+namespace BlindMatchPAS.Controllers;
+
+[Authorize(Roles = "SystemAdmin")]
+public class AdminController : Controller
 {
-    public class HomeController : Controller
+    private readonly ApplicationDbContext        _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public AdminController(ApplicationDbContext db,
+                           UserManager<ApplicationUser> userManager)
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+        _db          = db;
+        _userManager = userManager;
     }
-}
+
+    public async Task<IActionResult> Dashboard()
+    {
+        ViewData["UserCount"]    = await _db.Users.CountAsync();
+        ViewData["ProjectCount"] = await _db.Projects.CountAsync();
+        ViewData["MatchedCount"] = await _db.Projects
+            .CountAsync(p => p.Status == ProjectStatus.Matched);
+        return View();
+    }
+
+   
